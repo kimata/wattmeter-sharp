aa5900040cdb79fc3012f4cf71deda2daaf9a6af
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-09-07 00:53:22 +0900

    doc: スクリプトに合わせて微修正

--- script/registry_push.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-NAME=wattmeter_sharp
-REGISTRY=registry.green-rabbit.net/library
-
-git push
-docker build . -t ${NAME}
-docker tag ${NAME} ${REGISTRY}/${NAME}
-docker push ${REGISTRY}/${NAME}
